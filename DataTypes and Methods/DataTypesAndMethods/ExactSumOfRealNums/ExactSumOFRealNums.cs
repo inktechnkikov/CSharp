924e0a348f6a382082831c9e009a0170391b7c21
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-01 01:40:10 +0300

    ExactSumOfRealNums

---
+++ DataTypes and Methods/DataTypesAndMethods/ExactSumOfRealNums/ExactSumOFRealNums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExactSumOfRealNums
+{
+    class ExactSumOFRealNums
+    {
+        static void Main()
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                decimal simpleNum = decimal.Parse(Console.ReadLine());
+                sum += simpleNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
