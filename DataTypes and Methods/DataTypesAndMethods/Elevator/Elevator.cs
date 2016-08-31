1e080fca220a23e9555a8befc37ec39fd5809c85
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-01 01:55:12 +0300

    Elevator

---
+++ DataTypes and Methods/DataTypesAndMethods/Elevator/Elevator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Elevator
+    {
+        static void Main()
+        {
+            // int elevateN = int.Parse(Console.ReadLine());
+            // int capacity = int.Parse(Console.ReadLine());
+            // double courses = Math.Ceiling((double)elevateN / capacity);
+            // Console.WriteLine("{0}",courses);
+            double elevateN = double.Parse(Console.ReadLine());
+            double capaxity = double.Parse(Console.ReadLine());
+            double courses = ((int)elevateN / capaxity);
+            Console.WriteLine("{0}",Math.Ceiling(courses));
+        }
+    }
+}
