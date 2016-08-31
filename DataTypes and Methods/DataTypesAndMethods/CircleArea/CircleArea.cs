c4ec8d2e907118d348c5629d2648d79516a6dd10
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-31 16:54:18 +0300

    CircleArea

---
+++ DataTypes and Methods/DataTypesAndMethods/CircleArea/CircleArea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class CircleArea
+    {
+        static void Main()
+        {
+            result();
+        }
+       public static double result()
+        {
+            double r = double.Parse(Console.ReadLine());
+            double area = r * r * Math.PI;
+            Console.WriteLine("{0:F12}",area);
+            return area;
+        }
+    }
+}
