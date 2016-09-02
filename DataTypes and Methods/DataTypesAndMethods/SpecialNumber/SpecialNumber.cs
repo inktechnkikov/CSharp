e056ae59902cd81aa89eb497e54eaea597bef51d
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-02 04:30:24 +0300

    update

---
+++ DataTypes and Methods/DataTypesAndMethods/SpecialNumber/SpecialNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialNumber
+{
+    class SpecialNumber
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+                while(digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                bool speacial = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 10);
+                Console.WriteLine("{0} -> {1}",i,speacial);
+            }
+        }
+    }
+}
---
+++ DataTypes and Methods/DataTypesAndMethods/Triple/TripleSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triple
+{
+    class TripleSum
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= input-1; i++)
+            {
+                for (int a = 0; a <= input-1; a++)
+                {
+                    for (int q = 0; q <=input-1; q++)
+                    {
+                        char letter = (char)('a' + i);
+                        char letter1 = (char)('a' + a);
+                        char letter2 = (char)('a' + q);
+
+                        Console.WriteLine("{0}{1}{2}", letter, letter1, letter2);
+                    }
+                }
+            }
+        }
+    }
+}
