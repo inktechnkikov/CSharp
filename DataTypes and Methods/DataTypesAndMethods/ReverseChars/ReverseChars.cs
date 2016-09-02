e093b18c47542d7cafd1f08cc689c795ef430574
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-02 23:06:26 +0300

    update

---
+++ DataTypes and Methods/DataTypesAndMethods/ReverseChars/ReverseChars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseChars
+{
+    class ReverseChars
+    {
+        static void Main()
+        {
+            int n = 3;
+            char[] box = new char[n];
+            for (int i = 0; i < box.Length; i++)
+            {
+                char letters = char.Parse(Console.ReadLine());
+                box[i] += letters;
+            }
+            Array.Reverse(box);
+            Console.Write(box);
+        }
+    }
+}
