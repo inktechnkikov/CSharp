3c5b3c9dc9f35e260dd28177bafb59e17bb48b74
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-04 17:52:56 +0300

    Update

---
+++ Arrays/InsertElementsInArr/insertElementsInArr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertElementsInArr
+{
+    class insertElementsInArr
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] ArrSize = new int[n];
+            for (int i = 0; i < ArrSize.Length; i++)
+            {
+                Console.WriteLine("insert number:");
+                ArrSize[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(String.Join(", ",ArrSize));
+        }
+    }
+}
