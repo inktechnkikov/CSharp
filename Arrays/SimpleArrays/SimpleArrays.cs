9f61291c7e41d7b812b52b5304825132106d0b54
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-04 17:42:47 +0300

    SimpleArrays

---
+++ Arrays/SimpleArrays/SimpleArrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArrays
+{
+    class SimpleArrays
+    {
+        static void Main()
+        {
+            int[] arr = new int[5];
+            arr[0] = 3;
+            arr[3] = 1;
+            arr[1] = 2;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
