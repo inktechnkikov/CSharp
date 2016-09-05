cf2cabf0f0923c0acd36b08dd206caae8766373f
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-05 19:05:06 +0300

    update

---
+++ Arrays/DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfWeek
+{
+    class DayOfWeek
+    {
+        static void Main()
+        {
+            string[] Days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int day = int.Parse(Console.ReadLine());
+            if(day >=1 && day <= 7)
+            {
+                Console.WriteLine(Days[day-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day");
+            }
+        }
+    }
+}
