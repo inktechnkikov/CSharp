8d7f5e7c7e78cbc6f5f2da86197dd4c1ac08f46a
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-05 20:34:26 +0300

    update

---
+++ Arrays/Array - fast way/array - fast way.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array___fast_way
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
