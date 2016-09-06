17756db373d6114cb0d5562373638799476c327f
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-09-06 14:14:14 +0300

    update

---
+++ Arrays/CondenceArr/CondecneArr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondenceArr
+{
+    class CondecneArr
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            while(nums.Length > 1)
+            {
+                int[] res = new int[nums.Length - 1];
+                for (int i = 0; i < nums.Length-1; i++)
+                {
+                    res[i] = nums[i] + nums[i + 1];
+                }
+                nums = res;
+            }
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
---
+++ Arrays/TripleSum/TripleSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleSum
+{
+    class TripleSum
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int b=i+1; b <nums.Length; b++)
+                {
+                    int a = nums[i];
+                    int h = nums[b];
+                    int sum = a + h;
+                    if(nums.Contains(sum))
+                    {
+                        Console.WriteLine($"{a} + {b} == {sum}");
+                    }
+                }
+            }
+            if(count == 0)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
